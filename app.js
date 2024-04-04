0221fd6582c70545b5a1db23efc54ac7cfa602e5
XxDavexXX <102324849+XxDavexXX@users.noreply.github.com>
2024-04-04 11:28:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 13304;
+
+const clientes = ['Dave', 'Alexander', 'Yeffer'];
+const productos = ['Laptop', 'Mesa', 'Fideos'];
+
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a la aplicación Express Dave!');
+});
+
+app.get('/clientes', (req, res) => {
+  res.json(clientes);
+});
+
+app.get('/productos', (req, res) => {
+  res.json(productos);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
